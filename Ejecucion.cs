e27c81c8e8b9dcb6a71f01524720017c42ad3de7
marvic7 <marco.cevallos@ister.edu.ec>
2023-08-04 22:58:14 -0500

    cambios ejecucion

--- Ejecucion.cs
+++ Ejecucion.cs
@@ -17,6 +17,13 @@ namespace Cuenta
             persona.setEdad(29);
             persona.setIdentificacion("1724525801");
             persona.ImprimirDatos();
+            Persona persona2 = new Persona();
+            persona2.setNombre("Marco Vinicio");
+            persona2.setApellido("Cevallos");
+            persona2.setEdad(44);
+            persona2.setIdentificacion("171661445");
+            persona2.ImprimirDatos();
+
         }
     }
 }
